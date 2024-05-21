b36a69983c4dfdc3499c3769f1658fc31772fa46
Emad <emadsiddiq@berkeley.edu>
2024-05-21 08:45:10 -0700

    email functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 from fastapi import FastAPI, Form, File, UploadFile
-#from mail import send_mail
+from mail import send_mail
 
 app = FastAPI()
 
@@ -12,19 +12,21 @@ async def login(firstname: Annotated[str, Form()],
                 cv: UploadFile):
     
     #Write file to read as attachment when sending mail
-    cv_bytes = await cv.read()
-    write_file(cv_bytes)
+    write_file('resume', await cv.read())
 
-    return {"firstname": firstname, 
+    send_mail('Alma', [email], 'Thanks for your interest!', 'Hi, thanks for signing up.', ['resume'])
+    send_mail('Alma', ['emadalma40@gmail.com'], 'New lead' + str(email), 'There is a new lead', ['resume'])
+
+    return {
+            "firstname": firstname, 
             "lastname": lastname,
-            "email": email,
-            "file_size": cv.filename, 
-            "bytes": str(cv_bytes)}
+            "email": email
+            }
 
 
 app.mount("/", StaticFiles(directory="static",html = True), name="static")
 
 
-def write_file(bytes):
-    with open('resume', 'wb') as binary_file:
+def write_file(filename, bytes):
+    with open(filename, 'wb') as binary_file:
         binary_file.write(bytes)
